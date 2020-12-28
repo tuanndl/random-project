6bac4325f26d4cbdd7f1fe9722bc6a7897d8bdc8
rockmusic <tuan2887903@gmail.com>
2020-12-28 22:45:39 +0700

    ignore .next

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c5d5b9a81541b9e7bf66";
+/******/ 	var hotCurrentHash = "9505d2a6b237621202dc";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
